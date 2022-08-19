cb726baa0714f466392ad0487e2d22974a9c37e5
CKawara <ndutakawara2@gmail.com>
2022-08-19 14:14:51 +0300

    add logout and hambuger menu

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -10,7 +10,7 @@ const NavBar = () => {
             <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
              <NavLink className="px-3 py-2 flex items-center text-lg uppercase font-bold leading-snug text-dark hover:opacity-75" to='/home'>E-nventory</NavLink>
               <button
-                className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+                className="text-dark cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
                 type="button"
                 onClick={() => setNavbarOpen(!navbarOpen)}
               >
@@ -28,14 +28,19 @@ const NavBar = () => {
             >
               <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
                 <li className="nav-item">
-                <NavLink className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-dark hover:opacity-75" to='/home'>Assets</NavLink>
+                <NavLink className="px-4 py-2 flex items-center text-xs uppercase font-bold leading-snug text-dark hover:opacity-75" to='/home'>Assets</NavLink>
                 </li>
                 <li className="nav-item">
-                <NavLink className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-dark hover:opacity-75" to='/dashboard'>Dashboard</NavLink>
+                <NavLink className="px-4 py-2 flex items-center text-xs uppercase font-bold leading-snug text-dark hover:opacity-75" to='/dashboard'>Dashboard</NavLink>
                 </li>
                 <li className="nav-item">
-                <NavLink className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-dark hover:opacity-75" to='#home'>Name</NavLink>
+                <p className="px-4 flex items-center text-xs  font-bold leading-snug text-dark hover:opacity-75">Name <br/> Role</p>
                 </li>
+                <button className="px-4 py-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
+                    </svg>
+                </button>
               </ul>
             </div>
           </div>
