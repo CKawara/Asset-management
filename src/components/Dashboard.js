68bdef9691d1edab6c4450ab481b238dda459c21
Catherine Kawara <ndutakawara2@gmail.com>
2022-08-22 14:18:26 +0300

    add dashboard view

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -5,6 +5,11 @@ const Dashboard = () => {
     return (
         <div>
             <NavBar/>
+            <div className='relative flex flex-wrap items-center justify-between px-5'>
+                <p className='text-2xl'>Users</p>
+                <button type="button" class="inline-block px-6 py-2.5 bg-green-500 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-green-600 hover:shadow-lg focus:bg-green-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-green-700 active:shadow-lg transition duration-150 ease-in-out">Add Users</button>
+            </div>
+            <hr/>
         </div>
     )
 }
