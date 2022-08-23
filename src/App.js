1734e062f2f3534f81ba34008519a7b84757a614
+ <+>
2022-08-23 15:00:36 +0300

    Debug

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import './App.css';
-import { BrowserRouter, Route, Routes} from "react-router-dom";
-import NavBar from './components/NavBar';
-import Home from './components/Home';
-import Dashboard from './components/Dashboard';
+import { BrowserRouter} from "react-router-dom";
+//import NavBar from './components/NavBar';
+//import Home from './components/Home';
+//import Dashboard from './components/Dashboard';
 import Login from './components/Login/Login';
  
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
@@ -13,11 +13,7 @@ function App() {
     <BrowserRouter>
       <div className='App'>
       <Login />
-      <NavBar />
-      <Routes>
-        <Route path={'/home'}  element={<Home/>} exact/>
-        <Route path={'/dashboard'} element={<Dashboard/>} />
-      </Routes>
+     
       </div>
     </BrowserRouter>
   );
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,99 +1,156 @@
 import React, { useState } from "react";
-import loginImage from './loginImage.png';
 import './Login.css'
-import Form from 'react-bootstrap/Form';
-import InputGroup from 'react-bootstrap/InputGroup';
-import Button from 'react-bootstrap/Button';
-import Col from 'react-bootstrap/Col';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
+import styled, { keyframes, createGlobalStyle } from "styled-components";
 
- 
+const jump = keyframes`
+  from{
+    transform: translateY(0)
+  }
+  to{
+    transform: translateY(-3px)
+  }
+`;
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+    background: whitesmoke;
+  }
+
+  body, html, #root {
+    height: 100%;
+    font-family: -apple-system, Ubuntu , BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;;
+  }
+`;
+
+const Wrapper = styled.section`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  width: 100%;
+`;
+
+const Form = styled.form`
+margin: 0 auto;
+margin-top: 10%;
+width: 100%;
+max-width: 500px;
+padding: 1.3rem;
+display: flex;
+flex-direction: column;
+position: relative;
+`;
+
+
+const Input = styled.input`
+  max-width: 100%;
+  padding: 8px 13px;
+  background: #f9f9fa;
+  color: #f03d4e;
+  margin-bottom: 0.9rem;
+  border-radius: 4px;
+  outline: 0;
+  border: 1px solid rgba(245, 245, 245, 0.7);
+  font-size: 14px;
+  transition: all 0.3s ease-out;
+  box-shadow: 0 0 3px rgba(0, 0, 0, 0.1), 0 1px 1px rgba(0, 0, 0, 0.1);
+  :focus,
+  :hover {
+    box-shadow: 0 0 3px rgba(0, 0, 0, 0.15), 0 1px 5px rgba(0, 0, 0, 0.1);
+  }
+`;
+
+const Button = styled.button`
+  max-width: 100%;
+  padding: 7px 7px;
+  color: rgb(253, 249, 243);
+  font-weight: 600;
+  text-transform: uppercase;
+  background: #2CAE66;
+  border: none;
+  border-radius: 20px;
+  outline: 0;
+  cursor: pointer;
+  margin-top: 0.6rem;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.1), 0 1px 2px rgba(0, 0, 0, 0.1);
+  transition: all 0.3s ease-out;
+  :hover {
+    
+    animation: ${jump} 0.2s ease-out forwards;
+  }
+`;
+
+// const Title = styled.h1`
+// font-size: 2em;
+// font-weight: bold;
+// text-align: center;
+// color: #2CAE66;
+// text-shadow: 0 1px 3px rgba(0, 0, 0, 0.1), 0 1px 2px rgba(0, 0, 0, 0.1);
+// `;
+
 function Login({ onLogin }) {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [errors, setErrors] = useState([]);
-    const [isLoading, setIsLoading] = useState(false);
-  
-    function handleSubmit(e) {
-      e.preventDefault();
-      setIsLoading(true);
-      fetch("/login", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ email, password }),
-      }).then((r) => {
-        setIsLoading(false);
-        if (r.ok) {
-          r.json().then((user) => onLogin(user));
-        } else {
-          r.json().then((err) => setErrors(err.errors));
-        }
-      });
-    }
-    return (
-      <>
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-      <Container>
-       <div className='login-container'>
-        <Row className="justify-content-center">
-            <Col className="col-5">
-            <h1 className='page-header'>Hello There!</h1>
-            <img src={loginImage}  className="w-100" alt='Login'/>
-            </Col>
+  function handleSubmit(e) {
+    e.preventDefault();
+    setIsLoading(true);
+    fetch("/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    }).then((r) => {
+      setIsLoading(false);
+      if (r.ok) {
+        r.json().then((user) => onLogin(user));
+      } else {
+        r.json().then((err) => setErrors(err.errors));
+      }
+    });
+  }
 
-            <Col className="col-5">
-                <h1 className='page-header'>Login</h1>
-
-                <Form onSubmit={handleSubmit}>
-
-                <InputGroup className="mb-3">
-                <Form.Control
-                  placeholder="Email"
-                  aria-label="Email"
-                  aria-describedby="basic-addon1"
-                  type="email"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-              </InputGroup>
-
-            <br/>
-
-            <InputGroup className="mb-3">
-                <Form.Control
-                  placeholder="Password"
-                  aria-label="Password"
-                  aria-describedby="basic-addon1"
-                  type="password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-              </InputGroup>
-               
-                <br/>
-
-                
-                <Button variant="success" type='submit' size="lg" active>
-                {isLoading ? "Loading..." : "Submit"}
-                </Button>
-                  
-                </Form>
-                
-                <div>
-                {errors.map(err => {return (
-                <h3 className='errors'>{err}</h3>
-            )})}
-                </div>
-            </Col>
-            
-        </Row>
-    </div>
-    </Container>
+  return (
+    <>
+      <GlobalStyle />
+      <Wrapper>
+      
+        <Form onSubmit={handleSubmit}>
+        
+          <Input
+            type="email"
+            name="email"
+            placeholder='Email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <br/>
+          <Input
+            type="password"
+            name="password"
+            placeholder='Password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <br/>
+          <Button variant="success" type='submit' size="lg" active>
+          {isLoading ? "Loading..." : "Submit"}
+          </Button>
+        </Form>
+        <div>
+        {errors.map(err => {return (
+        <h3 className='errors'>{err}</h3>
+    )})}
+        </div>
+      </Wrapper>
     </>
-    )
+  );
 }
 
-export default Login
+export default Login;
