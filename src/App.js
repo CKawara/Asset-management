72e7a2c59810a0c028f95f23339babaa64296355
+ <+>
2022-08-24 11:15:25 +0300

    st:user-dashboard

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import './App.css';
 import { BrowserRouter, Route, Routes} from "react-router-dom";
-import NavBar from './components/NavBar';
+//import NavBar from './components/NavBar';
 import Home from './components/Home';
-import Dashboard from './components/Dashboard';
+//import Dashboard from './components/Dashboard';
 import Login from './components/Login/Login';
 import UserDashboard from './components/User-Dashboard/UserDashboard';
  
@@ -14,12 +14,12 @@ function App() {
     <BrowserRouter>
       <div className='App'>
      <Login/>
-      <NavBar />
+     
       <Routes>
         <Route path={'/home'}  element={<Home/>} exact/>
-        <Route path={'/dashboard'} element={<Dashboard/>} />
-        <Route path={'/userdashboard'} element={<UserDashboard/>} />
-      </Routes>
+     </Routes>
+    
+     <UserDashboard />
      
       </div>
     </BrowserRouter>
--- src/components/User-Dashboard/UserDashboard.js
+++ src/components/User-Dashboard/UserDashboard.js
@@ -1,23 +1,33 @@
 import React from "react";
+import styled from "styled-components";
 import NavBar from '../NavBar';
 import UserTable from './user_table';
 
 function UserDashboard() {
     return (
-        <>
+      <>
         <NavBar/>
+          <div className="Top">
+            <Title>
+              Asset Requests
+            </Title>
+          </div>
+        <Divider/>
         <UserTable/>
-        
-        
-        
-        
-        
-        
-        
-        
-        </>
-
-    );
+      </>
+        );
 }
+
+
+const Divider = styled.hr`
+  border: none;
+  border-bottom: 2px solid green;
+  margin: 20px 0;
+`;
+
+const Title = styled.h3`
+background: whitesmoke;
+  
+`;
 
 export default UserDashboard;
