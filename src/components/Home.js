a7adab8976e2c7b797f65cddabcd6ed0fb9e3cf0
Mwagiru <joymwagiru@gmail.com>
2022-08-24 12:14:01 +0300

    Create Assets Table, Asset Request component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import AssetsTable from './AssetsTable'
+import AssetRequest from './AssetRequest'
 
 const Home = () => {
     return (
-        <div className='min-h-screen'>
-            <AssetsTable/>
+      <div className="min-h-screen">
+        <div className="relative flex flex-wrap items-center justify-between px-5">
+          <p className="text-2xl">Assets Available</p>
+          <AssetRequest />
         </div>
-    )
+        <hr/>
+        <AssetsTable />
+      </div>
+    );
 }
 
 export default Home
