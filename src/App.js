a787009dfb312f01c6bbcf226dbf1fbd15a0b691
+ <+>
2022-08-24 10:26:22 +0300

    User dashboard

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import './App.css';
-import { BrowserRouter} from "react-router-dom";
-//import NavBar from './components/NavBar';
-//import Home from './components/Home';
-//import Dashboard from './components/Dashboard';
+import { BrowserRouter, Route, Routes} from "react-router-dom";
+import NavBar from './components/NavBar';
+import Home from './components/Home';
+import Dashboard from './components/Dashboard';
 import Login from './components/Login/Login';
+import UserDashboard from './components/User-Dashboard/UserDashboard';
  
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
@@ -12,7 +13,13 @@ function App() {
   return (
     <BrowserRouter>
       <div className='App'>
-      <Login />
+     <Login/>
+      <NavBar />
+      <Routes>
+        <Route path={'/home'}  element={<Home/>} exact/>
+        <Route path={'/dashboard'} element={<Dashboard/>} />
+        <Route path={'/userdashboard'} element={<UserDashboard/>} />
+      </Routes>
      
       </div>
     </BrowserRouter>
---
+++ src/components/User-Dashboard/UserDashboard.js
@@ -0,0 +1,23 @@
+import React from "react";
+import NavBar from '../NavBar';
+import UserTable from './user_table';
+
+function UserDashboard() {
+    return (
+        <>
+        <NavBar/>
+        <UserTable/>
+        
+        
+        
+        
+        
+        
+        
+        
+        </>
+
+    );
+}
+
+export default UserDashboard;
