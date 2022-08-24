11022746711da528149429b2d63b2d3d1071de29
Catherine Kawara <ndutakawara2@gmail.com>
2022-08-24 11:21:06 +0300

    fx:bugs

--- src/components/UsersTable.js
+++ src/components/UsersTable.js
@@ -127,9 +127,6 @@ const UsersTable = () => {
                   <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
                     Ross
                   </td>
-                  <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                    user
-                  </td>
                   
                 </tr>
               </tbody>
