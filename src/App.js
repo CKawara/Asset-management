9060560902523c852e877ea97566fed859f9a107
Git_TJ <98149148+GeoffreyGichaga@users.noreply.github.com>
2022-08-24 10:24:26 +0300

    upload login page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Routes} from "react-router-dom";
 import Home from './components/Home';
 import Dashboard from './components/Dashboard';
 import LandingPage from './components/LandingPage';
+import Login from './components/Login'
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path={'/'} element={<LandingPage/>}/>
         <Route path={'/home'}  element={<Home/>} exact/>
         <Route path={'/dashboard'} element={<Dashboard/>} />
+        <Route path={'/login'} element={<Login/>}/>
       </Routes>
       </div>
     </BrowserRouter>
---
+++ src/components/Login.js
@@ -0,0 +1,78 @@
+import React, { useState } from 'react'
+import '../Styling/Login.css'
+
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Button from 'react-bootstrap/Button'
+import man from '../Assets/man.png'
+import Form from 'react-bootstrap/Form'
+
+
+
+
+
+const Login = () => {
+    const [email,setEmail] = useState('')
+    const [password,setPassword] = useState('')
+
+    const handleSubmit = (e)=>{
+        e.preventDefault()
+
+        const user = {
+            email: email,
+            password: password
+        }
+
+        fetch('/users',{
+            method: "POST",
+            headers: {
+                'Content-Type':'application/json'
+            },
+            body: JSON.stringify(user)
+
+        })
+        .then(res=> res.json())
+        .then(data=> console.log(data))
+        
+    }
+
+  return (
+    <>  
+    <div id='login'>
+        <Container  >
+            <Row>
+                <Col sm={12} md={6} lg={6} className='left-column mt-5'>
+                    <h3 className='hello-text mt-3'>Hello There!</h3>
+                    <img src={man} alt='logo'/>
+
+                </Col>
+
+                <Col sm={12} md={6} lg={5} className='mt-5' id='login-form'>
+                <Form onSubmit={handleSubmit} id='log-form'>
+                    <h3 className='login-title text-center mb-3'>Login</h3>
+
+                    <Form.Group className='mb-4'>
+                        <Form.Control value={email} onChange={(e)=> setEmail(e.target.value)} className='input-fields' type='email' placeholder='Enter Email'/>
+                    </Form.Group>
+
+                    <Form.Group className=' mb-3'>
+                        <Form.Control value={password} onChange={(e)=> setPassword(e.target.value)} className='input-fields' type='password' placeholder='Enter Password'/>
+                    </Form.Group>
+
+                    <div className='d-flex justify-content-center'>
+                        <Button type='submit' id='submit-btn' className='mt-4'>Submit</Button>
+                    </div>
+                </Form>
+
+
+                </Col>
+            </Row>
+        </Container>
+        </div>
+
+    </>
+  )
+}
+
+export default Login
