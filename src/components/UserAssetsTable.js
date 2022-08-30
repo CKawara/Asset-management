22951e7f9207d88114459ef8f4364a3b0632422b
Catherine Kawara <ndutakawara2@gmail.com>
2022-08-30 10:11:51 +0300

    add link to users' assets

--- src/components/UserAssetsTable.js
+++ src/components/UserAssetsTable.js
@@ -2,6 +2,10 @@ import React from 'react'
 
 const UserAssetsTable = () => {
 
+  const getAsset = (e)=>{
+    console.log(e.target.parentNode.parentElement.childNodes[0].textContent);
+  }
+
   return (
     <>
       <div className="flex flex-col bg-white m-7 rounded-2xl drop-shadow-md p-3">
@@ -85,7 +89,27 @@ const UserAssetsTable = () => {
                       15 inches, corei5, 8GB RAM ...
                     </td>
                     <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                      <button className='bg-emerald-500 text-white px-2 py-2 rounded shadow-md hover:bg-green-500  transition duration-150 ease-in-out'>Request Asset</button> 
+                      <button onClick={getAsset} className='bg-emerald-500 text-white px-2 py-2 rounded shadow-md hover:bg-green-500  transition duration-150 ease-in-out'>Request Asset</button> 
+                    </td> 
+                  </tr>
+
+
+
+                  <tr className="border-b transition duration-300 ease-in-out hover:bg-gray-100">
+                    <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                      1
+                    </td>
+                    <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                      Hp15
+                    </td>
+                    <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                      Laptop
+                    </td>
+                    <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                      15 inches, corei5, 8GB RAM ...
+                    </td>
+                    <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                      <button onClick={getAsset} className='bg-emerald-500 text-white px-2 py-2 rounded shadow-md hover:bg-green-500  transition duration-150 ease-in-out'>Request Asset</button> 
                     </td> 
                   </tr>
                   
